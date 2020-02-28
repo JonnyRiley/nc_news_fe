98bf4093ac0b177fd41b16a8d9a64cb057646199
JonnyRiley <56361035+JonnyRiley@users.noreply.github.com>
2020-02-28 17:44:29 +0000

    tweak

--- fe-nc-news/src/Components/TopicTile.jsx
+++ fe-nc-news/src/Components/TopicTile.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import * as Api from "../Api";
-import IsLoading from "../Components/IsLoading";
-import Articles from "./Articles";
+
 import ErrorPage from "./ErrorPage";
 class TopicTile extends Component {
   state = {
@@ -24,33 +22,8 @@ class TopicTile extends Component {
             <option value="cooking">Cooking</option>
           </select>
         </main>
-        {/* {topics.map(topic => (
-          <ul key={topic.slug} className="li_topic_list">
-          <li>{topic.slug}</li>
-          </ul>
-        ))} */}
-        <main></main>
       </aside>
     );
   }
-  // getEachTopic = event => {
-  //   const { target } = event;
-  //   console.log(target.value, "TARGET");
-  //   return this.setState(currentState => {
-  //     return { filterTopicsBy: target.value };
-  //   });
-  // };
-  // componentDidMount() {
-  //   const { filterTopicsBy } = this.state;
-  //   console.log("mounting");
-  //   Api.FetchTopics(filterTopicsBy)
-  //     .catch(err => {
-  //       this.setState({ err });
-  //     })
-  //     .then(res => {
-  //       console.log(res);
-  //       this.setState({ topics: res, isLoading: false });
-  //     });
-  // }
 }
 export default TopicTile;
