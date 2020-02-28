83b0cf5dbb253fb9743ef640d71e52a2f4f2603f
JonnyRiley <56361035+JonnyRiley@users.noreply.github.com>
2020-02-28 17:54:51 +0000

    more console.logs removed

--- fe-nc-news/src/Api.jsx
+++ fe-nc-news/src/Api.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 const baseURL = "https://portfolio-nc-news.herokuapp.com/api";
 
 export const FetchArticles = (sortBy, filterTopicsBy) => {
-  console.log(filterTopicsBy, "FILTERing");
   return axios
     .get(baseURL + "/articles", {
       params: {
@@ -26,53 +25,43 @@ export const FetchArticleById = article_id => {
 };
 
 export const FetchCommentsByArticleId = article_id => {
-  console.log("FEtch");
   return axios.get(baseURL + `/articles/${article_id}/comments`).then(res => {
-    console.log(res, "FETCHING");
     return res.data.comments;
   });
 };
 
 export const FetchTopics = () => {
   return axios.get(baseURL + `/topics`).then(res => {
-    console.log(res, "FETCHING");
     return res.data.topics;
   });
 };
 
 export const postAnItem = (article_id, requestBody) => {
-  console.log(article_id, requestBody, "POSTER");
   return axios
     .post(baseURL + `/articles/${article_id}/comments`, requestBody)
     .then(({ data }) => {
-      console.log(data, "last");
       return data.comment;
     });
 };
 
 export const patchVotes = (inc_votes, comment_id) => {
-  console.log(inc_votes, comment_id, "handlingRequest");
   return axios.patch(baseURL + `/comments/${comment_id}`, {
     inc_votes: inc_votes
   });
 };
 
 export const patchArticleVotes = (inc_votes, article_id) => {
-  console.log(inc_votes, article_id, "handlingRequest");
   return axios.patch(baseURL + `/articles/${article_id}`, {
     inc_votes: inc_votes
   });
 };
 
 export const deleteCommentById = comment_Id => {
-  console.log(comment_Id, "handlingDELETE");
   return axios.delete(baseURL + `/comments/${comment_Id}`);
 };
 
 export const FetchUsers = username => {
-  console.log(username, "USERNAME");
   return axios.get(baseURL + `/users/${username}`).then(res => {
-    console.log(res.data);
     return res.data.user;
   });
 };
